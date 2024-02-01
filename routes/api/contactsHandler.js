3876732f5cb856dd299a588d4522aba21e60f571
jfrasinska <j.frasinska@gmail.com>
2024-02-01 22:39:41 +0100

    correcting errors

--- routes/api/contactsHandler.js
+++ routes/api/contactsHandler.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-const { nanoid } = require("nanoid");
+const { nanoid } = "nanoid";
 
 const contactsPath = path.join(__dirname, "../../contacts.json");
 

806a8c30641b5cc88fc96b3827ab12b89a81bb4a
jfrasinska <j.frasinska@gmail.com>
2024-02-07 00:04:25 +0100

    adding env with connection string

--- routes/api/contactsHandler.js
+++ routes/api/contactsHandler.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+require("dotenv").config();
 
 const contactSchema = new Schema({
   name: {
@@ -20,8 +21,7 @@ const contactSchema = new Schema({
 
 const Contact = mongoose.model("Contact", contactSchema);
 
-const connectionString =
-  "mongodb+srv://jfrasinska:WlazlKotekNaPlotek@cluster0.zoh3uaf.mongodb.net/contacts";
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
@@ -105,71 +105,3 @@ module.exports = {
   updateContact,
   updateStatusContact,
 };
-
-// const fs = require("fs").promises;
-// const path = require("path");
-// const { createHash } = require("crypto");
-
-// const contactsPath = path.join(__dirname, "../../contacts.json");
-
-// const readContacts = async () => {
-//   try {
-//     const data = await fs.readFile(contactsPath, "utf-8");
-//     return JSON.parse(data);
-//   } catch (error) {
-//     return [];
-//   }
-// };
-
-// const writeContacts = async (contacts) => {
-//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
-// };
-
-// const generateUniqueId = () => {
-//   const hash = createHash("sha256");
-//   hash.update(Math.random().toString());
-//   return hash.digest("hex");
-// };
-
-// const listContacts = async () => {
-//   return await readContacts();
-// };
-
-// const getById = async (id) => {
-//   const contacts = await readContacts();
-//   return contacts.find((contact) => contact.id === id);
-// };
-
-// const addContact = async (contact) => {
-//   const contacts = await readContacts();
-//   contact.id = generateUniqueId();
-//   contacts.push(contact);
-//   await writeContacts(contacts);
-//   return contact;
-// };
-
-// const removeContact = async (id) => {
-//   let contacts = await readContacts();
-//   contacts = contacts.filter((contact) => contact.id !== id);
-//   await writeContacts(contacts);
-//   return contacts.length !== contacts.length;
-// };
-
-// const updateContact = async (id, updatedFields) => {
-//   let contacts = await readContacts();
-//   const index = contacts.findIndex((contact) => contact.id === id);
-//   if (index !== -1) {
-//     contacts[index] = { ...contacts[index], ...updatedFields };
-//     await writeContacts(contacts);
-//     return contacts[index];
-//   }
-//   return null;
-// };
-
-// module.exports = {
-//   listContacts,
-//   getById,
-//   addContact,
-//   removeContact,
-//   updateContact,
-// };

d9720dd945ddeca9b425722aa4697bc569858a13
jfrasinska <j.frasinska@gmail.com>
2024-02-08 00:43:43 +0100

    step 5

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -132,4 +132,22 @@ router.get("/logout", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.get("/current", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    res.status(200).json({
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
